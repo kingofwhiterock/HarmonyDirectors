ac037b72bf76c9474fe00ab692a82d57fa5c7f57
Apoi <49585634+kingofwhiterock@users.noreply.github.com>
2020-06-29 12:12:17 +0900

    prepare docs and setup

---
+++ docs/example.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# !/usr/bin/env python3
+# --------------------------------------------------
+#
+# written by: Apoi
+# version: 0.1.0
+#
+# --------------------------------------------------
+#
+# PROJECT:
+# This is an example file of HarmonyDirectors.
+#
+# --------------------------------------------------
+
+# --------------------------------------------------
+# importing
+# --------------------------------------------------
+from harmonydirectors.chord import ChordLetters
+from harmonydirectors.sound import SoundGenerator
+from harmonydirectors.analysis import ChordAnalysis
+from harmonydirectors.audio import AudioController
+# --------------------------------------------------
+# class / code
+# --------------------------------------------------
